035b9e341ba632e0f369d5c1e725e8d15f85d014
SongKro <songkro48@gmail.com>
2020-06-25 23:43:51 +0700

    comment controller

---
+++ models/comment.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const { Model } = sequelize.Sequelize
+  class Comment extends Model {}
+  Comment.init({
+    song_id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER
+  },{
+    sequelize,modelName: 'Comment'
+  });
+  Comment.associate = function(models) {
+    // associations can be defined here
+    Comment.belongsTo(models.Song)
+    Comment.belongsTo(models.User)
+  };
+  return Comment;
+};
