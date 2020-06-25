1ea68fcf37cf093e46b7145360fb094c38b2d474
SongKro <songkro48@gmail.com>
2020-06-25 18:26:37 +0700

    controller done

--- controller/songController.js
+++ controller/songController.js
@@ -1,28 +1,49 @@
 const { Song } = require('../models')
 
 class SongController{
-    static findAll(req, res, next){
-        const { Title, Artist, Genre, comment } = req.body
-        console.log(req.body)
-        Song.findAll({ Title, Artist, Genre, comment })
-            .then(dataSong => {
-                res.status(200).json(dataSong)
-            })
-            .catch(err => {
-                res.status(400).json(err)
-            })
-    }
-    static create(req, res, next){
-        const { Title, Artist, Genre, comment } = req.body
-        Song.create({ Title, Artist, Genre, comment })
+    static read(req, res, next){
+        Song.findAll()
         .then( data => {
-            res.status(201).json(data)
+            res.status(200).json(data)
         })
         .catch( err => {
             res.status(400).json(err)
         })
     }
-    static 
+
+    static create(req, res, next){
+        const { Title, Artist, Genre, comments } = req.body
+        Song.create({ Title, Artist, Genre, comments })
+        .then( data => {
+            res.status(201).json({ msg: 'data song berhasil dibuat'})
+        })
+        .catch( err => {
+            res.status(400).json(err)
+        })
+    }
+    
+    static update(req, res){
+        const { Title } = req.body
+        const { id } = req.params
+        Song.update({ Title }, {where: { id }})
+        .then( data => {
+            res.status(200).json({ msg: 'Title berhasil diupdate'})
+        })
+        .catch( err => {
+            res.status(400).json(err)
+        })
+    }
+
+    static delete(req, res){
+        const { id } = req.params
+        Song.destroy({ where: { id }})
+        .then( data =>{
+            res.status(200).json({ msg: 'Song berhasil dihapus'})
+        })
+        .catch( err => {
+            res.status(400).json(err)
+        })
+    }
 }
 
 module.exports = SongController
--- routes/song.js
+++ routes/song.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const SongController = require('../controller/songController')
 
 
-router.post('/', SongController.create )
+router.post('/', SongController.create)
+router.get('/', SongController.read)
+router.put('/title/:id', SongController.update)
+router.delete('/:id', SongController.delete)
 
 module.exports = router
