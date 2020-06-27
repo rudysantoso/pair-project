4970904cf92706ea3240bdcfd95b367a81b092af
SongKro <songkro48@gmail.com>
2020-06-27 19:15:08 +0700

    comment done

--- routes/comment.js
+++ routes/comment.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 const CommentController = require('../controller/commentController')
 
 router.get('/', CommentController.read)
+router.post('/', CommentController.create)
+router.put('/comment/:id', CommentController.update)
+router.delete('/:id', CommentController.delete)
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const user = require('./user')
 const song = require('./song')
+const comment = require('./comment')
 
 router.use('/user', user)
 router.use('/song', song)
+router.use('/comment', comment)
 
 module.exports = router
